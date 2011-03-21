16735938dd448b6c11fc03c0a9ab37c393a317a3
drunkcod <i.am@drunkencoder.com>
2011-03-21 10:40:50 +0100

    First official release of SouthBurndown

--- Site/Controllers/ChartsController.cs
+++ Site/Controllers/ChartsController.cs
@@ -3,16 +3,18 @@ using System.Drawing;
 using System.Linq;
 using System.Web.Mvc;
 using CardWall.Models;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CardWall.Controllers
 {
     public class ChartsController : Controller
     {
         public ActionResult Index() {
-            var burndown = new BurndownData();
             var burndownColor = Color.SteelBlue;
 
-            var startDate = burndown.Data.Min(item => item.Date);
+            var burndown = GetSouthBurndownData();
+            var startDate = burndown.Min(item => item.Date);
             var endDate = new DateTime(2011, 6, 1);
 
             var totalDays = (int)Math.Ceiling((endDate - startDate).TotalDays);
@@ -20,15 +22,15 @@ namespace CardWall.Controllers
             var encoding = new GoogleExtendedEncoding();
             var maxValue = encoding.MaxValue;
             
-            var maxPoints = burndown.Data.Max(item => item.PointsRemaining);
+            var maxPoints = burndown.Max(item => item.PointsRemaining);
             var chartMax = (int)Math.Round(maxPoints * 1.1);
             var yAxis = new ChartAxis(Axis.Y, new Tuple<int, int>(0, chartMax), new string[0], new int[0]);
             var ys = new ChartSeries("", Color.Transparent, 
-                burndown.Data.Select(item => item.PointsRemaining).Scale(0, chartMax, 0, maxValue));
+                burndown.Select(item => item.PointsRemaining).Scale(0, chartMax, 0, maxValue));
 
             var xAxis = new ChartAxis(Axis.X, new Tuple<int, int>(0, 1), new[]{ startDate.ToShortDateString(), endDate.ToShortDateString() }, new[]{0, 1});
             var x = new ChartSeries("Points Remaining", burndownColor, 
-                burndown.Data.Select(item => (int)(item.Date - startDate).TotalDays).Scale(0, totalDays, 0, maxValue));
+                burndown.Select(item => (int)(item.Date - startDate).TotalDays).Scale(0, totalDays, 0, maxValue));
           
             var xBurnLine = new ChartSeries("", Color.FromArgb(128, Color.Firebrick), new []{ 0, maxValue });
             var yBurnLine = new ChartSeries("", Color.Transparent, new []{ maxValue, 0});
@@ -38,6 +40,30 @@ namespace CardWall.Controllers
                 ChartMarker.NewCircle(Color.White, 0, -1, 4)
             }, LineChartMode.XY, encoding, new[]{ LineStyle.Default, LineStyle.NewDashed(2, 2, 4) });
             return View(chart);
+        }
+
+        BurndownData GetSouthBurndownData() 
+        {
+            var tracker = new PivotalTracker(Environment.GetEnvironmentVariable("TrackerToken", EnvironmentVariableTarget.Machine));
+            var stories = 
+                tracker.Stories(173053).Result
+                .Where(item => item.Labels.Contains("team south", StringComparer.InvariantCultureIgnoreCase))
+                .Where(item => item.CurrentState != PivotalStoryState.Accepted);
+
+            var pointsRemaining = stories.Sum(item => Math.Max(0, item.Estimate ?? 0)); 
+
+            return new BurndownData(LoadBurndownData(Server.MapPath("TeamSouthBurndown.txt"))){
+                { DateTime.Today, pointsRemaining }
+            };
+        }
+
+        IEnumerable<BurndownDataPoint> LoadBurndownData(string path) {
+            using(var input = new StreamReader(System.IO.File.OpenRead(path))) {
+                for(string line; (line = input.ReadLine()) != null;) {
+                    var parts = line.Split(';');
+                    yield return new BurndownDataPoint(DateTime.Parse(parts[0]), int.Parse(parts[1]));
+                }                    
+            }
         }
     }
 }
--- Site/Models/BurndownData.cs
+++ Site/Models/BurndownData.cs
@@ -4,36 +4,27 @@ using System.Linq;
 
 namespace CardWall.Models
 {
-    public class BurndownData 
+    public class BurndownData : IEnumerable<BurndownDataPoint>
     {
-        readonly int pointsRemaining;
+        readonly List<BurndownDataPoint> dataPoints = new List<BurndownDataPoint>();
 
-        public BurndownData() 
-        {
-            var tracker = new PivotalTracker(Environment.GetEnvironmentVariable("TrackerToken", EnvironmentVariableTarget.Machine));
-            var stories = 
-                tracker.Stories(173053).Result
-                .Where(item => item.Labels.Contains("team south", StringComparer.InvariantCultureIgnoreCase))
-                .Where(item => item.CurrentState != PivotalStoryState.Accepted);
-
-            pointsRemaining = stories.Sum(item => Math.Max(0, item.Estimate ?? 0)); 
+        public BurndownData() { }
+        public BurndownData(IEnumerable<BurndownDataPoint> data) {
+            dataPoints.AddRange(data);
         }
 
-        public int PointsRemaining { get { return pointsRemaining; } }
+        public int PointsRemaining { get { return dataPoints.Last().PointsRemaining; } }
 
-        public IEnumerable<BurndownDataPoint> Data 
-        {
-            get 
-            {
-                return new[]{
-                    new BurndownDataPoint(DateTime.Parse("2011-02-21"), 24)
-                    ,new BurndownDataPoint(DateTime.Parse("2011-02-28"), 16)
-                    ,new BurndownDataPoint(DateTime.Parse("2011-03-08"), 19)
-                    ,new BurndownDataPoint(DateTime.Parse("2011-03-14"), 25)
-                    ,new BurndownDataPoint(DateTime.Parse("2011-03-15"), 93)
-                    ,new BurndownDataPoint(DateTime.Today, pointsRemaining)
-                };
-            }
+        public void Add(DateTime date, int pointsRemaining) {
+            dataPoints.Add(new BurndownDataPoint(date, pointsRemaining));
+        }
+
+        IEnumerator<BurndownDataPoint> IEnumerable<BurndownDataPoint>.GetEnumerator() {
+            return dataPoints.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            return dataPoints.GetEnumerator();
         }
     }
 }
