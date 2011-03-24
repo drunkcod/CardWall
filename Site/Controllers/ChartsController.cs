2327843eff63bbdaa66d1eb2d21df8262ac2e6c3
drunkcod <i.am@drunkencoder.com>
2011-03-24 17:10:48 +0100

    Seeing the light?

--- Site/Controllers/ChartsController.cs
+++ Site/Controllers/ChartsController.cs
@@ -45,16 +45,11 @@ namespace CardWall.Controllers
             var xBurnLine = new ChartSeries("", Color.FromArgb(128, Color.Firebrick), new []{ 0, maxValue });
             var yBurnLine = new ChartSeries("", Color.Transparent, new []{ maxValue, 0});
 
-            var chart = new LineChart(string.Format("{0} points remaining", burndown.PointsRemaining), 800, 300, new []{ xAxis, yAxis }, new []{ x, ys, xBurnLine, yBurnLine}, new []{
+            var chart = new Chart(string.Format("{0} points remaining", burndown.PointsRemaining), 800, 300, new []{ xAxis, yAxis }, new []{ x, ys, xBurnLine, yBurnLine}, new []{
                 ChartMarker.NewCircle(burndownColor, 0, -1, 8),
                 ChartMarker.NewCircle(Color.White, 0, -1, 4)
-<<<<<<< HEAD
             }, ChartMode.XYLine, encoding, new[]{ LineStyle.Default, LineStyle.NewDashed(2, 2, 4) });
-            return View(new ChartView { Name = title, DisplayMarkup = "<img src='" + chart.ToString() + "'/>" });
-=======
-            }, LineChartMode.XY, encoding, new[]{ LineStyle.Default, LineStyle.NewDashed(2, 2, 4) });
             return View(new ChartView { Name = configuration.Name, DisplayMarkup = "<img src='" + chart.ToString() + "'/>" });
->>>>>>> ef2a1dd7fb738bd520e888784db9181aeca61350
         }
 
         BurndownData GetSouthBurndownData(int project, string label, string historicalDataPath) 
