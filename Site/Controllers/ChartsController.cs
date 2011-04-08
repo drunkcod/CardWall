674d73bed0534a5013a1c6ae162d9e16b1bc40d4
drunkcod <i.am@drunkencoder.com>
2011-04-08 11:26:30 +0200

    Velocity and Scope curves added.

--- Site/Controllers/ChartsController.cs
+++ Site/Controllers/ChartsController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Web.Mvc;
 using CardWall.Models;
@@ -21,6 +19,7 @@ namespace CardWall.Controllers
         public ActionResult Index(string team) {
             var burndownColor = Color.SteelBlue;
             var velocityColor = Color.YellowGreen;
+            var scopeColor = Color.Orange;
 
             var configuration = GetConfiguration(team);
             
@@ -33,33 +32,37 @@ namespace CardWall.Controllers
             var encoding = new GoogleExtendedEncoding();
             var maxValue = encoding.MaxValue;
             
-            var maxPoints = burndown.Max(item => item.PointsRemaining);
+            var maxPoints = burndown.Max(item => item.TotalPoints);
             var chartMax = (int)Math.Round(maxPoints * 1.1);
             var yAxis = new ChartAxis(Axis.Y, new Tuple<int, int>(0, chartMax), new string[0], new int[0]);
+            var velocityAxis = new ChartAxis(Axis.Right, new Tuple<int, int>(0, 20), new []{ "0", "5", "10", "15", "Velocity" }, new []{ 0, 5, 10, 15, 20 });
             
             var pointsRemaining = new ChartSeries("", Color.Transparent, burndown.Select(item => item.PointsRemaining).Scale(0, chartMax, 0, maxValue));
-            var velocity = new ChartSeries("", Color.Transparent, burndown.Select(item => item.TotalPointsBurned).Scale(0, chartMax, 0, maxValue));
+            var velocity = new ChartSeries("", Color.Transparent, burndown.Select(item => item.Velocity).Scale(0, 25, 0, maxValue));
+            var scope = new ChartSeries("", Color.Transparent, burndown.Select(item => item.TotalPoints).Scale(0, chartMax, 0, maxValue));
 
             var xAxis = new ChartAxis(Axis.X, new Tuple<int, int>(0, 1), new[]{ startDate.ToShortDateString(), endDate.ToShortDateString() }, new[]{0, 1});
             var xs = burndown.Select(item => (int)(item.Date - startDate).TotalDays).Scale(0, totalDays, 0, maxValue);
 
             var pointsRemainingSeries = new ChartSeries("Points Remaining", burndownColor, xs);
             var velocitySeries = new ChartSeries("Velocity", velocityColor, xs);
+            var scopeSeries = new ChartSeries("Scope", scopeColor, xs);
           
             var xBurnLine = new ChartSeries("", Color.FromArgb(128, Color.Firebrick), new []{ 0, maxValue });
             var yBurnLine = new ChartSeries("", Color.Transparent, new []{ maxValue, 0});
 
             var chart = new Chart(
-                string.Format("{0} points remaining", burndown.PointsRemaining), 800, 300,
-                new []{ xAxis, yAxis }, 
+                string.Format("{0} points remaining. Velocity {1}", burndown.PointsRemaining, burndown.Velocity), 800, 300,
+                new []{ xAxis, yAxis, velocityAxis }, 
                 new [] { 
                     pointsRemainingSeries, pointsRemaining,
                     velocitySeries, velocity,
+                    scopeSeries, scope, 
                     xBurnLine, yBurnLine
                 }, new []{
                 ChartMarker.NewCircle(burndownColor, 0, MarkerPoints.All, 8),
                 ChartMarker.NewCircle(Color.White, 0, MarkerPoints.All, 4)
-            }, ChartMode.XYLine, encoding, new[]{ LineStyle.Default, LineStyle.Default, LineStyle.NewDashed(2, 2, 4)});
+            }, ChartMode.XYLine, encoding, new[]{ LineStyle.Default, LineStyle.Default, LineStyle.Default, LineStyle.NewDashed(2, 2, 4)});
             return View(new ChartView { Name = configuration.Name, DisplayMarkup = "<img src='" + chart.ToString() + "'/>" });
         }
 
@@ -74,9 +77,10 @@ namespace CardWall.Controllers
             var pointsRemaining = stories.Where(item => item.CurrentState != PivotalStoryState.Accepted).Sum(item => Math.Max(0, item.Estimate ?? 0)); 
             var totalPointsBurned = stories.Where(item => item.CurrentState == PivotalStoryState.Accepted).Sum(item => Math.Max(0, item.Estimate ?? 0)); 
 
-            return new BurndownData(LoadBurndownData(Server.MapPath(historicalDataPath))){
-                { DateTime.Today, pointsRemaining, totalPointsBurned }
-            };
+            var burndownData = new BurndownData();
+            burndownData.Load(Server.MapPath(historicalDataPath));
+            burndownData.Add(DateTime.Today, pointsRemaining, totalPointsBurned);
+            return burndownData;
         }
 
         BurndownChartConfiguration GetConfiguration(string team) {
@@ -102,14 +106,6 @@ namespace CardWall.Controllers
                         Label = "team south",
                         HistoricalDataPath = "TeamSouthBurndown.txt"
                     };
-            }
-        }
-
-        IEnumerable<BurndownDataPoint> LoadBurndownData(string path) {
-            using(var input = new StreamReader(System.IO.File.OpenRead(path))) {
-                for(string line; (line = input.ReadLine()) != null;) {
-                    yield return BurndownDataPoint.Parse(line);
-                }                    
             }
         }
     }
--- Site/Models/BurndownData.cs
+++ Site/Models/BurndownData.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace CardWall.Models
 {
-    public class BurndownData : IEnumerable<BurndownDataPoint>
+    public class BurndowChartData 
+    {
+        public DateTime Date;
+        public int PointsRemaining;
+        public int Velocity;
+        public int TotalPoints;
+    }
+
+    public class BurndownData : IEnumerable<BurndowChartData>
     {
         readonly List<BurndownDataPoint> dataPoints = new List<BurndownDataPoint>();
 
@@ -13,18 +22,42 @@ namespace CardWall.Models
             dataPoints.AddRange(data);
         }
 
+        public void Load(string path) {
+            using(var input = new StreamReader(System.IO.File.OpenRead(path))) {
+                for(string line; (line = input.ReadLine()) != null;) {
+                    dataPoints.Add(BurndownDataPoint.Parse(line));
+                }                    
+            }
+        }
+
         public int PointsRemaining { get { return dataPoints.Last().PointsRemaining; } }
+
+        public int Velocity { get { return GetChartData().ElementAt(dataPoints.Count - 3).Velocity; } }
 
         public void Add(DateTime date, int pointsRemaining, int totalPointsBurned) {
             dataPoints.Add(new BurndownDataPoint(date, pointsRemaining, totalPointsBurned));
         }
 
-        IEnumerator<BurndownDataPoint> IEnumerable<BurndownDataPoint>.GetEnumerator() {
-            return dataPoints.GetEnumerator();
+        IEnumerator<BurndowChartData> IEnumerable<BurndowChartData>.GetEnumerator() {
+            return GetChartData().GetEnumerator();
         }
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
-            return dataPoints.GetEnumerator();
+            return GetChartData().GetEnumerator();
         }
+
+        IEnumerable<BurndowChartData> GetChartData() {
+            for(var i = 1; i < dataPoints.Count; ++i) {
+                var x = dataPoints[i - 1];
+                var y = dataPoints[i];
+                yield return new BurndowChartData { 
+                    Date = y.Date,
+                    PointsRemaining = y.PointsRemaining,
+                    Velocity = y.TotalPointsBurned - x.TotalPointsBurned,
+                    TotalPoints = y.PointsRemaining + y.TotalPointsBurned
+                };
+            }
+        }
+
     }
 }
